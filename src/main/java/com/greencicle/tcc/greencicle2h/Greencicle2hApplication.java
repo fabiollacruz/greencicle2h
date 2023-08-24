d2dc7d8e6b7a7bc1a1fe1e7b44ef059a3dd0af15
fabiollacruz <fabiollapereiracruz123@gmail.com>
2023-08-24 11:04:18 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/greencicle/tcc/greencicle2h/Greencicle2hApplication.java
@@ -0,0 +1,13 @@
+package com.greencicle.tcc.greencicle2h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Greencicle2hApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Greencicle2hApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greencicle/tcc/greencicle2h/Greencicle2hApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greencicle.tcc.greencicle2h;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Greencicle2hApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
